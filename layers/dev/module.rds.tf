7e2c879862d3f0d42e456ee5835fc93a95e6b8c9
Nagarjuna <46648905+nags28@users.noreply.github.com>
2019-07-30 12:34:22 +0530

    Update module.rds.tf

--- layers/dev/module.rds.tf
+++ layers/dev/module.rds.tf
@@ -1,7 +1,7 @@
 module "rds" {
   source = "../../modules/rds"
   region = "${var.region}"
-   availability_zones = "us-east-2a"
+   //availability_zones = "us-east-2a"
    engine = "${var.engine}"
     engine_version = "${var.engine_version}"
     instance_class = "${var.instance_class}"
